75598111b2228af14632aaa69346fecd627e5bfc
THOMAS MORIN <tomamorin@gmail.com>
2016-05-26 17:04:22 +0200

    PREMIER COMMIT

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Titre</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+
+    <header></header>
+    <h1>METTTEZ LES PETITS PLATS DANS LES GRANDS</h1>
+
+        <!-- Formulaire -->
+        <form action="index.php" method="post">
+
+            <div class="block">
+                <p>Code Postal :</p>
+                <input class="postal" type="text" name="postal">
+                <input class="valid" type="submit" value="COMMANDER">
+            </div>
+        </form>
+
+    <footer></footer>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery-2.2.4.min.js"></script>
+
+</body>
+</html>
