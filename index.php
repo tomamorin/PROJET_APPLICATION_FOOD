d19ffde34bc13323a292ff6c56167a387d869679
THOMAS MORIN <tomamorin@gmail.com>
2016-05-31 14:35:07 +0200

    commit page chargement et création page formule

--- index.php
+++ index.php
@@ -10,18 +10,17 @@
 </head>
 
 <body>
-<script>$(window).load(function(){ jQuery("loader").fadeOut(1.2000); });</script>
-
+<script src="js/jquery-2.2.4.min.js"></script>
+<script>jQuery(window).load(function(){ jQuery(".loader").delay(4000).fadeOut(500); });</script>
     <header></header>
     <div class="loader">
-        <h1>VEUillEZ PATIENTE</h1>
+        <h1>VEUIllEZ PATIENTEZ</h1>
     </div>
     <h1>METTTEZ LES PETITS PLATS DANS LES GRANDS</h1>
 
         <!-- Formulaire -->
-        <form action="index.php" method="post">
 
-            <div class="bloock">
+            <div class="block">
                 <p>Code Postal :</p>
                 <input class="postal" type="text" name="postal">
                 <input class="valid" type="submit" value="COMMANDER">
@@ -34,7 +33,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="js/bootstrap.min.js"></script>
-<script src="js/jquery-2.2.4.min.js"></script>
 
 </body>
 </html>
