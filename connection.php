1a3bcd3587415b068f137d76ebf9ffc010d6d750
Theov <theo.voison@gmail.com>
2016-06-02 21:57:20 +0200

    début de connection / inscription

--- connection.php
+++ connection.php
@@ -1,7 +1,37 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Theo
- * Date: 02/06/2016
- * Time: 18:20
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Titre</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery-2.2.4.min.js"></script>
+
+</body>
+</html>
